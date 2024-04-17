96745b9afe69cc9263665e10d837a099f82f46f4
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2024-04-17 19:47:16 -0400

    made chatgpt endpoint

--- index.js
+++ index.js
@@ -5,24 +5,23 @@ const {parse, stringify, toJSON, fromJSON} = require('flatted');
 require("dotenv").config()
 const axios = require('axios') 
 
-const {Configuration, OpenAIApi} = require("openai")
+const {OpenAI} = require("openai")
 
-const config = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY,
-})
+const openai = new OpenAI({
+    api_key: process.env.OPEN_AI_KEY,
+  });
 
-const openai = new OpenAIApi(config)
-
-app.get('/', async (req, res) => {
+app.get('/chatgpt', async (req, res) => {
   try {
-    const response = await openai.createCompletion({
-        model:"gpt-4-turbo",
-        prompt:"what is 11 plus 14",
-
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ 
+        role: 'user', 
+        content: 'Review this interview response to \"What is your greatest weakness?\": \"My greatest weakness is that I am too smart\""' }],
+        model: 'gpt-3.5-turbo',
     });
     
 
-    res.send(result);
+    res.send(chatCompletion);
   } catch(e) {
     // catch errors and send error status
     console.log(e);
@@ -32,5 +31,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`DMOJ-Assistant listening at http://localhost:${port}`);
+  console.log(`Interview-Assistant listening at http://localhost:${port}`);
 });
